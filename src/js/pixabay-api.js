fe3fb92c8d16b5613221c7ca372a80d7dfef6413
Oleksandr Kostiuchenko <aleksandrkostucenko11@gmail.com>
2025-01-25 08:27:25 +0100

    add-pagination

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -6,47 +6,60 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+const loadMoreBtn = document.querySelector('.load-more-btn');
+
+import axios from "axios";
 
 //* Export fecth function
-export const fetchImg = (userQuery, gallery, queryInput) => {
+export const fetchImg = async (userQuery, gallery, queryInput, page) => {
     const mainEl = document.querySelector('main');
     mainEl.insertAdjacentHTML('beforeend', `<span class="loader"></span>`)
 
     const pageLoader = document.querySelector('.loader');
 
-    fetch(`https://pixabay.com/api/?q=${userQuery}&key=39635982-91d856b8fc78635a8aaf79b21&image_type=photo&orientation=horizontal&safesearch=true`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
+    const galleryData = await axios.get(`https://pixabay.com/api/?q=${userQuery}&key=39635982-91d856b8fc78635a8aaf79b21&image_type=photo&orientation=horizontal&safesearch=true&per_page=15&page=${page}`)
 
-            pageLoader.remove();
-            gallery.innerHTML = '';
-            queryInput.value = '';
-            return response.json();
-        })
-        .then(imagesArr => {
-            if (imagesArr.total === 0) {
-                iziToast.error({
-                    title: 'Error',
-                    titleSize: '25',
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                    messageSize: '15',
-                    position: 'bottomRight'
-                });
-            }
+    pageLoader.remove();
+    gallery.innerHTML = '';
+    queryInput.value = '';
 
-            let galleryHTML = [];
-            imagesArr.hits.map(imageEl => {
-                galleryHTML.push(renderGallery(imageEl));
-            })
+    if (galleryData.data.total === 0) {
+        iziToast.error({
+            title: 'Error',
+            titleSize: '25',
+            message: 'Sorry, there are no images matching your search query. Please try again!',
+            messageSize: '15',
+            position: 'bottomRight'
+        });
+        return;
+    }
 
-            gallery.insertAdjacentHTML('beforeend', galleryHTML.join(' '));
-            
-            const lightbox = new SimpleLightbox('.gallery-list a', {
-                captionSelector: 'img',
-                captionsData: 'alt',
-                captionDelay: 250,
-            });
-        })
+    let galleryHTML = [];
+    galleryData.data.hits.map(imageEl => {
+        galleryHTML.push(renderGallery(imageEl));
+    })
+
+    gallery.insertAdjacentHTML('beforeend', galleryHTML.join(' '));
+
+    const lightbox = new SimpleLightbox('.gallery-list a', {
+        captionSelector: 'img',
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+
+    return galleryData;
+}
+
+//* Export Load more function
+export const loadMore = async (userQuery, gallery, queryInput, page) => {
+    const mainEl = document.querySelector('main');
+    mainEl.insertAdjacentHTML('beforeend', `<span class="loader"></span>`)
+
+    const pageLoader = document.querySelector('.loader');
+
+    const galleryData = await axios.get(`https://pixabay.com/api/?q=${userQuery}&key=39635982-91d856b8fc78635a8aaf79b21&image_type=photo&orientation=horizontal&safesearch=true&per_page=15&page=${page}`)
+
+    pageLoader.remove();
+
+    return galleryData;
 }
--- src/main.js
+++ src/main.js
@@ -7,19 +7,28 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import axios from "axios";
+
 //* Import modules
 import { fetchImg } from "./js/pixabay-api";
+import { renderGallery } from "./js/render-functions";
+import { loadMore } from "./js/pixabay-api";
 
 //* Find elements
 const queryForm = document.querySelector('.user-query-form'); 
 const queryInput = document.querySelector('.user-query-input'); 
 const queryBtn = document.querySelector('.user-query-btn');
 const gallery = document.querySelector('.gallery-list');
+const loadMoreBtn = document.querySelector('.load-more-btn');
+
+let userQuery;
+let page = 1;
+
 
 //* Add event listener & apply modules
-const onFormSubmit = event => {
+const onFormSubmit = async event => {
     event.preventDefault();
-    const userQuery = queryInput.value.trim();
+    userQuery = queryInput.value.trim();
 
     if (userQuery === '') {
         iziToast.error({
@@ -33,7 +42,37 @@ const onFormSubmit = event => {
         return;
     }
 
-    fetchImg(userQuery, gallery, queryInput);
+    loadMoreBtn.classList.add('is-hidden');
+    page = 1;
+    const { data } = await fetchImg(userQuery, gallery, queryInput, page);
+    console.log(data);
+
+    if (data.totalHits > 1) {
+        loadMoreBtn.classList.remove('is-hidden');
+        
+        loadMoreBtn.addEventListener('click', onLoadMoreClick)
+    }
+}
+
+const onLoadMoreClick = async event => {
+    page++;
+    
+    const { data } = await loadMore(userQuery, gallery, queryInput, page);
+
+    const galleryTemplate = data.hits.map(el => renderGallery(el)).join('');
+
+    gallery.insertAdjacentHTML('beforeend', galleryTemplate);
+
+    const lightbox = new SimpleLightbox('.gallery-list a', {
+        captionSelector: 'img',
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+
+    if (data.hits.length === 0) {
+        loadMoreBtn.classList.add('is-hidden')
+        loadMoreBtn.removeEventListener('click', onLoadMoreClick)
+    }
 }
 
 queryForm.addEventListener('submit', onFormSubmit);
